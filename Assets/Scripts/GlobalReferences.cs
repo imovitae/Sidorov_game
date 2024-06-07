ad8cfc56938624a84ddf7d470fb8a0d5a1956c9e
imovitae <168636050+imovitae@users.noreply.github.com>
2024-06-07 17:41:59 +0300

    Update GlobalReferences.cs

--- Assets/Scripts/GlobalReferences.cs
+++ Assets/Scripts/GlobalReferences.cs
@@ -1,1 +1,27 @@
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalReferences : MonoBehaviour
+{
+    public static GlobalReferences Instance { get; set; }//тип экземпляра 
+
+    public GameObject bulletImpactEffectPrefab;
+    private void Awake()
+    {
+        //проверка экземпляра | синголтон | мы хотим иметь только один экземпляр глобал. ссылок 
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+
+
+}
